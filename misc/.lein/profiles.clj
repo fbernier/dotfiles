740d51a1f22a6320446e71f9c5ac3f6e61f87dca
Francois Bernier <frankbernier@gmail.com>
2015-03-06 10:22:44 -0500

    backup commit before I break everything

--- misc/.lein/profiles.clj
+++ misc/.lein/profiles.clj
@@ -1,1 +1,1 @@
-{:user {:plugins [[lein-simpleton "1.2.0"] [lein-exec "0.3.2"] [lein-try "0.4.1"] [lein-ancient "0.5.4"] [lein-kibit "0.0.8"] [jonase/eastwood "0.1.0"]]}}
+{:user {:plugins [[lein-simpleton "1.3.0"] [lein-exec "0.3.4"] [lein-try "0.4.3"] [lein-ancient "0.6.3"] [lein-kibit "0.0.8"] [jonase/eastwood "0.2.1"]]}}
