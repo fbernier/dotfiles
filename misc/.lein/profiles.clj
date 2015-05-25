95502a363033e801fa06fcf902ee1d5c69b3f756
Francois Bernier <frankbernier@gmail.com>
2015-05-25 15:07:36 -0400

    stuff

--- misc/.lein/profiles.clj
+++ misc/.lein/profiles.clj
@@ -1,1 +1,1 @@
-{:user {:plugins [[lein-simpleton "1.3.0"] [lein-exec "0.3.4"] [lein-try "0.4.3"] [lein-ancient "0.6.3"] [lein-kibit "0.0.8"] [jonase/eastwood "0.2.1"]]}}
+{:user {:plugins [[lein-simpleton "1.3.0"] [lein-exec "0.3.4"] [lein-try "0.4.3"] [lein-ancient "0.6.7"] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.1"]]}}
